248cd9ab1ad632e590fc1e25dd0dff6c6c63355b
MichalNebesky <91117510+MichalNebesky@users.noreply.github.com>
2022-01-07 19:00:15 +0100

    Verze 1

---
+++ script.js
@@ -0,0 +1,40 @@
+$(document).ready(function(){
+$(".carousel").owlCarousel({
+    margin: 20,
+    loop: true,
+    autoplay: true,
+    autoplayTimeout: 2000,
+    autoplayHoverPause: true,
+    responsive: {
+      0:{
+        items:1,
+        nav: false
+      },
+      600:{
+        items:2,
+        nav: false
+      },
+      1000:{
+        items:3,
+        nav: false
+      }
+    }
+  });
+
+  var owl = $(".owl-carousel");
+  owl.owlCarousel({
+    items: 1,
+    loop: true,
+    dots:false,
+    autoplay:true,
+    autoPlayTimeout: 5000,
+  });
+  function rollOut() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
+});
